01e9dd4f1ff5c3ca5575513df157ee870364560a
Justin Washington <jwashington@mobiquityinc.com>
2016-04-27 15:45:09 -0400

    add android build tools to dependencies

---
+++ src/main/groovy/info/solidsoft/gradle/pitest/PitestPluginAndroid.groovy
@@ -0,0 +1,15 @@
+package info.solidsoft.gradle.pitest
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by jwashington on 4/27/16.
+ */
+class PitestPluginAndroid implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
