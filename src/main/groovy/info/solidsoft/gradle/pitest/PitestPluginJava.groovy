add2c58636a7fa77451abf34d8fc40374eb5ccd5
Justin Washington <jwashington@mobiquityinc.com>
2016-04-27 15:40:17 -0400

    separate java pitest plugin implementation from base plugin

---
+++ src/main/groovy/info/solidsoft/gradle/pitest/PitestPluginJava.groovy
@@ -0,0 +1,161 @@
+/* Copyright (c) 2012 Marcin Zajączkowski
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package info.solidsoft.gradle.pitest
+
+import groovy.transform.PackageScope
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+import org.gradle.api.logging.Logger
+import org.gradle.api.logging.Logging
+import org.gradle.api.plugins.JavaBasePlugin
+import org.gradle.api.file.FileCollection
+import org.gradle.api.internal.file.UnionFileCollection
+import com.google.common.annotations.VisibleForTesting
+
+/**
+ * The main class for Pitest plugin.
+ */
+class PitestPluginJava implements Plugin<Project> {
+    public final static DEFAULT_PITEST_VERSION = '1.1.9'
+    public final static PITEST_TASK_GROUP = "Report"
+    public final static PITEST_TASK_NAME = "pitest"
+    public final static PITEST_CONFIGURATION_NAME = 'pitest'
+
+    private final static Logger log =  Logging.getLogger(PitestPluginJava)
+
+    @VisibleForTesting
+    @PackageScope
+    final static String PIT_HISTORY_DEFAULT_FILE_NAME = 'pitHistory.txt'
+
+    private Project project
+    private PitestPluginExtension extension
+
+    void apply(Project project) {
+        this.project = project
+        applyRequiredJavaPlugin()
+        createConfigurations()
+        createExtension(project)
+        project.plugins.withType(JavaBasePlugin) {
+            PitestTask task = project.tasks.create(PITEST_TASK_NAME, PitestTask)
+            task.with {
+                description = "Run PIT analysis for java classes"
+                group = PITEST_TASK_GROUP
+            }
+            configureTaskDefault(task)
+        }
+    }
+
+    private void applyRequiredJavaPlugin() {
+        //The new Gradle plugin mechanism requires all mandatory plugins to be applied explicit
+        //See: https://github.com/szpak/gradle-pitest-plugin/issues/21
+        project.apply(plugin: 'java')
+    }
+
+    private void createConfigurations() {
+        project.rootProject.buildscript.configurations.maybeCreate(PITEST_CONFIGURATION_NAME).with {
+            visible = false
+            description = "The Pitest libraries to be used for this project."
+        }
+    }
+
+    //TODO: MZA: Maybe move it to the constructor of an extension class?
+    private void createExtension(Project project) {
+        extension = project.extensions.create("pitest", PitestPluginExtension)
+        extension.reportDir = new File("${project.reporting.baseDir.path}/pitest")
+        extension.pitestVersion = DEFAULT_PITEST_VERSION
+        extension.testSourceSets = [project.sourceSets.test] as Set
+        extension.mainSourceSets = [project.sourceSets.main] as Set
+    }
+
+    private void configureTaskDefault(PitestTask task) {
+        task.conventionMapping.with {
+            taskClasspath = {
+                List<FileCollection> testRuntimeClasspath = extension.testSourceSets*.runtimeClasspath
+
+                FileCollection combinedTaskClasspath = new UnionFileCollection(testRuntimeClasspath)
+                FileCollection combinedTaskClasspathWithoutPomFiles = combinedTaskClasspath.filter { File file -> !file.name.endsWith(".pom") }
+                combinedTaskClasspathWithoutPomFiles
+            }
+            launchClasspath = {
+                project.rootProject.buildscript.configurations[PITEST_CONFIGURATION_NAME]
+            }
+            mutableCodePaths = { (extension.mainSourceSets*.output.classesDir.flatten() as Set) + (extension.additionalMutableCodePaths ?: []) }
+            sourceDirs = { extension.mainSourceSets*.allSource.srcDirs.flatten() as Set }
+
+            reportDir = { extension.reportDir }
+            targetClasses = {
+                log.debug("Setting targetClasses. project.getGroup: {}, class: {}", project.getGroup(), project.getGroup()?.class)
+                if (extension.targetClasses) {
+                    return extension.targetClasses
+                }
+                if (project.getGroup()) {   //Assuming it is always a String class instance
+                    return [project.getGroup() + ".*"] as Set
+                }
+                null
+            }
+            targetTests = { extension.targetTests }
+            dependencyDistance = { extension.dependencyDistance }
+            threads = { extension.threads }
+            mutateStaticInits = { extension.mutateStaticInits }
+            includeJarFiles = { extension.includeJarFiles }
+            mutators = { extension.mutators }
+            excludedMethods = { extension.excludedMethods }
+            excludedClasses = { extension.excludedClasses }
+            avoidCallsTo = { extension.avoidCallsTo }
+            verbose = { extension.verbose }
+            timeoutFactor = { extension.timeoutFactor }
+            timeoutConstInMillis = { extension.timeoutConstInMillis }
+            maxMutationsPerClass = { extension.maxMutationsPerClass }
+            childProcessJvmArgs = { extension.jvmArgs }
+            outputFormats = { extension.outputFormats }
+            failWhenNoMutations = { extension.failWhenNoMutations }
+            includedGroups = { extension.includedGroups }
+            excludedGroups = { extension.excludedGroups }
+            detectInlinedCode = { extension.detectInlinedCode }
+            timestampedReports = { extension.timestampedReports }
+            historyInputLocation = { extension.historyInputLocation }
+            historyOutputLocation = { extension.historyOutputLocation }
+            enableDefaultIncrementalAnalysis = { extension.enableDefaultIncrementalAnalysis }
+            defaultFileForHistoryDate = { new File(project.buildDir, PIT_HISTORY_DEFAULT_FILE_NAME) }
+            mutationThreshold = { extension.mutationThreshold }
+            mutationEngine = { extension.mutationEngine }
+            coverageThreshold = { extension.coverageThreshold }
+            exportLineCoverage = { extension.exportLineCoverage }
+            jvmPath = { extension.jvmPath }
+            mainProcessJvmArgs = { extension.mainProcessJvmArgs }
+            pluginConfiguration = { extension.pluginConfiguration }
+        }
+
+        project.afterEvaluate {
+            task.dependsOn(calculateTasksToDependOn())
+
+            addPitDependencies()
+        }
+    }
+
+    private Set<String> calculateTasksToDependOn() {
+        Set<String> tasksToDependOn = extension.testSourceSets.collect { it.name + "Classes" }
+        log.debug("pitest tasksToDependOn: $tasksToDependOn")
+        tasksToDependOn
+    }
+
+    private void addPitDependencies() {
+        project.rootProject.buildscript.dependencies {
+            log.info("Using PIT: $extension.pitestVersion")
+            pitest "org.pitest:pitest-command-line:$extension.pitestVersion"
+        }
+    }
+}
